17de79efa24f09879e74fbfeb92eb94c75693978
trbenne2 <luvsrub@Tylers-MacBook-Pro-2.local>
2020-04-18 19:59:56 -0400

    connected prisma to server and graphql schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
-
+const { prisma } = require('./generated/prisma-client');
 /**
  * Defines GraphQL schema. Simple query type with 1 field info with
  * Type String that is required
@@ -22,14 +22,7 @@ type Link {
 `; */
 
 // storing this link at runtime
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL',
-  },
-];
-let idCount = links.length;
+
 /**
  * The actual implementation of the GraphQL schema.
  * Structure is identical to the structure of the type def
@@ -38,28 +31,16 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     // root field for feed
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent, args) => {
-      const link = links.map((item) => item.id === args.id);
-      link.url = args.url;
-      link.description = args.description;
-      return link;
-    },
-    deleteLink: (parent, args) => {
-      const link = links.map((item) => item.id === args.id);
-      links.splice(link, 1);
-      return link;
+        description: args.description,
+      });
     },
   },
 };
@@ -71,5 +52,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
